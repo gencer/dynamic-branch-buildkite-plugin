eedd888eabdd22f82db6d30f1fc1d3d60df2fcf0
Bryan Wong <bryanwong@improbable.io>
2020-06-23 13:50:52 +0100

    pull docker from repo

--- helper/common.sh
+++ helper/common.sh
@@ -1,22 +1,38 @@
 #!/usr/bin/env bash
 
-function build_and_run_docker
+TAG="dynamic-branch"
+REMOTE_TAG="${BUILDKITE_PLUGIN_DYNAMIC_BRANCH_DOCKER_TAG:-bryan2402/bk-plugins:${TAG}}"
+
+function build_and_push_docker
 {
-	local dockerfile="$1"
-	local docker_tag="dynamic-branch"
-	local bk_dir="$2" 
+	local DOCKERFILE_PATH="${1:?Expecting 1st argument to be a Dockerfile path}"
+	docker build -f "$DOCKERFILE_PATH" -t "$TAG" .
+    docker tag "$TAG" "$REMOTE_TAG"
+    docker push "$REMOTE_TAG"
+}
 
-    docker build -f "$dockerfile" -t "$docker_tag" . >&2
-
-    docker run $(printenv | sed -n "s/\(^BUILDKITE[^=]*\).*/--env \1/gp") --env BK_DIR="$bk_dir" -v "${bk_dir}:/bk" -t "$docker_tag"
+function pull_and_run_docker
+{
+	local BK_DIR="${1:?Expecting 1st argument to be a directory}"
+    docker run $(printenv | sed -n "s/\(^BUILDKITE[^=]*\).*/--env \1/gp") --env BK_DIR="$BK_DIR" -v "${BK_DIR}:/bk" "$REMOTE_TAG"
 }
 
 function upload_pipeline
 {
-	local file_path="$1"
-	if grep -q "^steps:[[:space:]]*$" "$file_path"; then
-        echo "--- Uploaded"
-        cat "$file_path"
-        buildkite-agent pipeline upload "$file_path"
+	if grep -q "^steps:[[:space:]]*$" "$EXE_BRANCH"; then
+        echo "--- Uploaded" && cat "$EXE_BRANCH"
+        buildkite-agent pipeline upload "$EXE_BRANCH"
+    fi
+}
+
+function run_plugin
+{
+	plugin_root="${1:?Expecting 1st argument to be a directory}"
+	set -a
+    eval $(pull_and_run_docker "${plugin_root}/.buildkite")
+    set +a
+
+    if [[ -f "$EXE_BRANCH" ]]; then
+        upload_pipeline "$EXE_BRANCH" && rm "$EXE_BRANCH"
     fi
 }
